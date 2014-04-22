dc12087930ad346f1330878e62639d9e9c8b628d
Badboum <alexandre-marion@hotmail.fr>
2014-04-22 16:12:38 +0200

    aaa
    aaaa

---
+++ src/main/java/fr/esir/lsi2/tpmaven/App.java
@@ -0,0 +1,13 @@
+package fr.esir.lsi2.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
